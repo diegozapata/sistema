de2c33fc1bc2c6767aa6b58afe3b4c447604da22
JavierYanezSepulveda <javierandres.y.s@gmail.com>
2018-11-21 16:24:36 -0300

    Create crear_ucc.php

---
+++ vistas principales/crear_ucc.php
@@ -0,0 +1,33 @@
+<div class="container"  >
+		<div class="row" >
+			
+			
+			<div class="col s12 m4 offset-m4 login" style="margin-top: 10%; margin-right: 30%; margin-left: 13% ">
+				<div class="col s12">
+					
+					<div class="col s12">
+						<!-- <h1></h1> -->
+						<h2 class="center" style="font-size: 22px;"> Crear nueva unidad de centro de costos </h2>	
+					</div>
+						
+					
+				</div>
+				<form action="index.php/ucc/crear_ucc" method="post">
+					<div class="col s12">
+						<label for="">Nombre de la nueva unidad</label>
+						<h1></h1>
+						<input type="text" name="nombre" id="nombre"  placeholder="Ej: Ingenieria Informática">
+					</div>
+					<div class="col s12">
+						<label for="">Anexo</label>
+						<h1></h1>
+						<input type="text" name="anexo" id="anexo"  placeholder="1717">
+					</div>
+					<div class="col s12">
+						<input type="submit" value="crear" class="btn UCM " style=" width: 100%; margin-bottom: 25px;">
+					</div>
+				</form>
+				
+			</div>
+		</div>
+	</div>
