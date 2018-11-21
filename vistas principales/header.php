5a1e0a25f4bd1b21b8214f845f99320ef53b753f
JavierYanezSepulveda <javierandres.y.s@gmail.com>
2018-11-21 14:48:15 -0300

    vistas y assets

---
+++ vistas principales/header.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+      <link rel="stylesheet" href="<?php echo base_url();?>/assets/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+      <link rel="stylesheet" type="text/css" media="screen" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+      <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url();?>/assets/css/materialize.css" />
+      <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url();?>/assets/css/animate.css" />
+      <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url();?>/assets/css/sweetalert.css" />
+      <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url();?>/assets/css/menu.css" />
+      <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url();?>/assets/css/notificaciones.css" />  
+      <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url();?>/assets/css/jquery.dataTables.min.css">
+      <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url();?>/assets/css/tableexport.css" /> 
+      <link rel="stylesheet" type="text/css" media="screen" href="<?php echo base_url();?>/assets/css/app.css" />
+      <link rel="stylesheet" type="text/css" media="print" href="<?php echo base_url();?>/assets/css/print.css" /> <link rel="stylesheet" type="text/css" media="print" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.0.3/sweetalert2.css" />   
+      <link href="<?php echo base_url();?>assets/css/dashboard.css" rel="stylesheet">
+      <link rel="stylesheet" href="<?php echo base_url();?>/assets/css/profile.css"/>
+
+    <title>Bienvenido</title>
+  </head>
+  <body>
+	<nav class="navbar navbar-expand-lg navbar-blue bg-white fixed-top">
+      <div class="container">
+        <a class="navbar" href="http://localhost/CIBOO">
+          <img src="http://portal.ucm.cl//content/uploads/2016/10/logo.png" width="150" height="30" alt="">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+       <!-- <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Services</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact</a>
+            </li>
+          </ul>
+        </div>-->
+      </div>
+    </nav>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="<?php echo base_url();?>/assets/js/jquery-3.3.1.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="<?php echo base_url();?>/assets/js/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="<?php echo base_url();?>/assets/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+ <script src="<?php echo base_url();?>/assets/js/jquery.js"></script>      
+      <script src="<?=$base_url?>/ajax/ajax.js"></script>
+      <script src="<?php echo base_url();?>/assets/js/materialize.js"></script>
+      <script src="<?php echo base_url();?>/assets/js/sweetalert.min.js"></script>
+      <script src="<?php echo base_url();?>/assets/js/smooth-scroll.js"></script>     
+      <script src="<?php echo base_url();?>/assets/js/wow.min.js"></script>
+      <script src="<?php echo base_url();?>/assets/js/jquery.dataTables.min.js"></script>
+        <script src="<?php echo base_url();?>/assets/js/notif.js"></script>
+        <script src="<?php echo base_url();?>/assets/js/xlsx.core.js"></script>
+        <script src="<?php echo base_url();?>/assets/js/FileSaver.js"></script>
+        <script src="<?php echo base_url();?>/assets/js/tableexport.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.0.3/sweetalert2.all.js"></script>
+            <!-- Icons -->
+    <script src="<?php echo base_url();?>/assets/js/feather.min.js"></script>
+    <script src="<?php echo base_url();?>/assets/js/Chart.min.js"></script>
---
+++ vistas principales/menu_lateral.php
@@ -0,0 +1,142 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    
+
+    <title>Dashboard Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+
+    
+  </head>
+
+  <body>
+    
+
+    <div class="container-fluid"  >
+
+      <div class="row" >
+        <nav class="sidebar">
+          <div class="sidebar-sticky" style="margin-right: 85%">
+            <ul class="nav flex-column">
+              <li class="nav-item">
+                <a class="nav-link active" href="#" >
+                  <span data-feather="home"></span>
+                  Dashboard <span class="sr-only">(current)</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#" >
+                  <span data-feather="file"></span>
+                  Ventas
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#" >
+                  <span data-feather="file"></span>
+                  Ordenes
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#" >
+                  <span data-feather="file"></span>
+                  Compras
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#" >
+                  <span data-feather="file"></span>
+                  Informes
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#" >
+                  <span data-feather="layers"></span>
+                  Insumos
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#" >
+                  <span data-feather="layers"></span>
+                  Productos
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#" >
+                  <span data-feather="layers"></span>
+                  Provedores
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#" >
+                  <span data-feather="layers"></span>
+                  UCC
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#" >
+                  <span data-feather="settings"></span>
+                  Configuraciones
+                </a>
+              </li>
+            </ul>
+
+
+          </div>
+        </nav>
+
+        
+      </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="../../assets/js/vendor/popper.min.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+
+    <!-- Icons -->
+    <script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
+    <script>
+      feather.replace()
+    </script>
+
+    <!-- Graphs -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js"></script>
+    <script>
+      var ctx = document.getElementById("myChart");
+      var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+          datasets: [{
+            data: [15339, 21345, 18483, 24003, 23489, 24092, 12034],
+            lineTension: 0,
+            backgroundColor: 'transparent',
+            borderColor: '#007bff',
+            borderWidth: 4,
+            pointBackgroundColor: '#007bff'
+          }]
+        },
+        options: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: false
+              }
+            }]
+          },
+          legend: {
+            display: false,
+          }
+        }
+      });
+    </script>
+  </body>
+</html>
